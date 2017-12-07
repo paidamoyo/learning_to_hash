5b461e3862112b6621d8f23be4e4f3d58c157b32
Paidamoyo <ashgirlchapfuwa@gmail.com>
2017-12-07 15:04:53 -0500

    RESULTS: SGH_16 stochastic'

--- flags_parameters.py
+++ flags_parameters.py
@@ -9,7 +9,7 @@ def set_params():
     flags.DEFINE_integer("learning_rate", 1e-2, "optimizer learning rate")
     flags.DEFINE_float("beta1", 0.9, "optimizer beta 1")
     flags.DEFINE_float("beta2", 0.999, "optimizer beta 2")
-    flags.DEFINE_integer("latent_dim", 32, "latent dimensions of z")
+    flags.DEFINE_integer("latent_dim", 16, "latent dimensions of z")
     flags.DEFINE_float("l2_reg", 1e-3, "l2 regularization weight multiplier (just for debugging not optimization)")
     flags.DEFINE_float('alpha', 1e-3, "cross entropy loss weight")
     flags.DEFINE_integer('queries', 10000, "size of test queries")
